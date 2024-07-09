3b7c79efca696acbe49ef1afe35fbb258567b40e
Miguel Bandeira <61510901+miguelgbandeira@users.noreply.github.com>
2024-07-09 18:01:10 +0100

    Add alert dialog

--- app/issues/[id]/issue-buttons.tsx
+++ app/issues/[id]/issue-buttons.tsx
@@ -1,3 +1,14 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 import { Button } from "@/components/ui/button";
 import { SquarePen, Trash2 } from "lucide-react";
 import Link from "next/link";
@@ -19,14 +30,29 @@ export function IssueEditButton({ issueId }: { issueId: string }) {
 
 export function IssueDeleteButton({ issueId }: { issueId: string }) {
   return (
-    <Button variant={"destructive"}>
-      <Trash2 className="mr-2 h-4 min-w-4" />
-      <Link className="block sm:hidden" href={`/issues/${issueId}/edit`}>
-        Delete Issue
-      </Link>
-      <Link className="hidden sm:block" href={`/issues/${issueId}/edit`}>
-        Delete
-      </Link>
-    </Button>
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button variant={"destructive"}>
+          <Trash2 className="mr-2 h-4 min-w-4" />
+          <div className="block sm:hidden">Delete Issue</div>
+          <div className="hidden sm:block">Delete</div>
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete this
+            issue and remove the data from our servers.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction className="bg-red-500 hover:bg-red-400">
+            Continue
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 }
