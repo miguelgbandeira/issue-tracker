a69ba8ab5870466650db4fbf603f80c7a9eafdfd
Miguel Bandeira <61510901+miguelgbandeira@users.noreply.github.com>
2024-07-09 11:44:56 +0100

    Update issues button functionality

--- app/issues/_components/issue-form.tsx
+++ app/issues/_components/issue-form.tsx
@@ -12,6 +12,7 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Issue } from "@prisma/client";
 import "easymde/dist/easymde.min.css";
 import dynamic from "next/dynamic";
 import { useRouter } from "next/navigation";
@@ -25,7 +26,7 @@ const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
 
 type IssueFormData = z.infer<typeof issueSchema>;
 
-export default function IssueForm({ issue }: { issue?: IssueFormData }) {
+export default function IssueForm({ issue }: { issue?: Issue }) {
   const form = useForm<IssueFormData>({
     resolver: zodResolver(issueSchema),
   });
@@ -35,13 +36,24 @@ export default function IssueForm({ issue }: { issue?: IssueFormData }) {
   const onSubmit = form.handleSubmit(async (data) => {
     try {
       setIsSubmitting(true);
-      const response = await fetch("/api/issues", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      });
+      let response;
+      if (issue) {
+        response = await fetch("/api/issues/" + issue.id, {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        });
+      } else {
+        response = await fetch("/api/issues", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        });
+      }
 
       if (!response.ok) {
         throw new Error("Failed to submit the issue");
@@ -88,7 +100,9 @@ export default function IssueForm({ issue }: { issue?: IssueFormData }) {
           )}
         />
         {!isSubmitting ? (
-          <Button type="submit">Submit</Button>
+          <Button type="submit">
+            {issue ? "Update Issue" : "Submit New Issue"}
+          </Button>
         ) : (
           <ButtonLoading />
         )}
