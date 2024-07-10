725c94289fb129dc361495ccb9540c8f34b7f4df
Miguel Bandeira <61510901+miguelgbandeira@users.noreply.github.com>
2024-07-10 14:42:52 +0100

    Add dropdown menu from user avatar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -2,9 +2,18 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-import { AiFillBug } from "react-icons/ai";
 import clsx from "clsx";
 import { useSession } from "next-auth/react";
+import { AiFillBug } from "react-icons/ai";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export default function Navbar() {
   const currentPath = usePathname();
@@ -40,7 +49,21 @@ export default function Navbar() {
           </div>
           <div>
             {status === "authenticated" && (
-              <Link href="/api/auth/signout">Sign Out</Link>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Avatar className="w-8 h-8 cursor-pointer">
+                    <AvatarImage src={session.user!.image!} />
+                    <AvatarFallback>?</AvatarFallback>
+                  </Avatar>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="w-56">
+                  <DropdownMenuLabel>{session.user!.email}</DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem>
+                    <Link href="/api/auth/signout">Sign Out</Link>
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
             )}
             {status === "unauthenticated" && (
               <Link href="/api/auth/signin">Sign In</Link>
